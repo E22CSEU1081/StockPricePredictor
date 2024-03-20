35a0659b0ad6a08bc40ba03622398070fdb1667e
Swapnil <145461592+E22CSEU1081@users.noreply.github.com>
2024-03-21 02:07:38 +0530

    Add files via upload

---
+++ stock_data_download_auto.py
@@ -0,0 +1,32 @@
+import datetime
+import pandas as pd
+from tqdm import tqdm
+import pickle
+#from numba import jit, cuda
+import yfinance as yf
+
+def getHistoricalData(stock_name):
+    stock_name = stock_name + '.NS'
+    time_interval = '3y'
+    stock = yf.Ticker(stock_name)
+    stock_data = stock.history(period=time_interval)
+    print('sdcs')
+    if stock_data.empty:
+        return stock_data
+    stock_data.index = stock_data.index.date
+    return stock_data
+
+def run():
+    stock_symbols = pd.read_csv('NIFTY_100.csv').loc[:,'Symbol']
+    print(stock_symbols)
+
+    data = {}
+
+    for i in tqdm(range(len(stock_symbols))):
+        d = getHistoricalData(stock_symbols.iloc[i])
+        if(not d.empty):
+            data[stock_symbols.iloc[i]] = d
+        
+
+    return  data
+
